75848270c164d143c11cf7d358594f30b7afd4aa
Evan Wills <evan.wills@acu.edu.au>
2020-02-01 19:20:28 +1100

    add action creators for all-player actions.

--- state/actions/player.action.ts
+++ state/actions/player.action.ts
@@ -1,3 +1,5 @@
+import { IAction } from "../types"
+
 export const PLAYER = {
   ADD: 'Add player to game',
   UPDATE: 'Update game player',
@@ -11,7 +13,7 @@ export const ALL_PLAYERS = {
   DELETE: 'Remove player from list'
 }
 
-export const addPlayerActionCreator = (_name, _position) => {
+export const addPlayerToGameActionCreator = (_name, _position) : IAction => {
   return {
     type: PLAYER.ADD,
     payload: {
@@ -21,7 +23,7 @@ export const addPlayerActionCreator = (_name, _position) => {
   }
 }
 
-export const updatePlayerActionCreator = (_id, _name, _position) => {
+export const updatePlayerActionCreator = (_id, _name, _position) : IAction => {
   return {
     type: PLAYER.UPDATE,
     payload: {
@@ -42,14 +44,56 @@ export const updatePlayerActionCreator = (_id, _name, _position) => {
  *
  * @returns {object}
  */
-export const scoreTurnActionCreator = (_id, _score, _endTime, _pausedTime) => {
+export const scoreTurnActionCreator = (_score : number) : IAction => {
   return {
     type: PLAYER.SCORE_TURN,
     payload: {
-      id: _id,
-      score: _score,
-      endTime: _endTime,
-      paused: _pausedTime
+      score: _score
+    }
+  }
+}
+
+/**
+ * Add a new player to the system.
+ * (This is not the same as adding a player to a game)
+ *
+ * @param _name name of the player being added to the system
+ */
+export const addNewPlayerActionCreator = (_name : string) => {
+  return {
+    type: ALL_PLAYERS.ADD,
+    payload: {
+      score: _name
+    }
+  }
+}
+
+/**
+ * Change the name of an existing player
+ * (This is not the same as adding a player to a game)
+ *
+ * @param _name name of the player being added to the system
+ */
+export const updateExistingPlayerActionCreator = (_name : string) => {
+  return {
+    type: ALL_PLAYERS.UPDATE,
+    payload: {
+      score: _name
+    }
+  }
+}
+
+/**
+ * Remove an existing player from the system
+ * (This means )
+ *
+ * @param _name name of the player being added to the system
+ */
+export const deleteExistingPlayerActionCreator = (_id : number) => {
+  return {
+    type: ALL_PLAYERS.UPDATE,
+    payload: {
+      score: _id
     }
   }
 }
