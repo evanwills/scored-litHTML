8b2c393bbcf1b8909504b18c593d80efd6fa0c70
Evan Wills <evan.wills@acu.edu.au>
2020-02-01 19:22:50 +1100

    improving structure.

--- state/middleware/round.middleware.ts
+++ state/middleware/round.middleware.ts
@@ -1,10 +1,11 @@
 import { TURN, endTurnActionCreator, startTurnActionCreator } from '../actions/turns.action'
 import { ROUND, initialiseRoundActionCreator, finaliseRoundActionCreator } from '../actions/round.action'
 import { getTotalScore } from '../reducers/scores.reducer'
-import { IGame } from '../types'
+import { IAction, IWholeScored } from '../types'
 
-export const roundMiddleWare = (store) => (next) => (action) => {
-  const currentState : IGame = store.getState()
+export const roundMiddleWare = (store) => (next) => (action : IAction) => {
+  const currentState : IWholeScored = store.getState()
+  const { config, end, players, round, scores } = currentState.currentGame
 
   switch (action.type) {
     case TURN.SCORE:
@@ -20,13 +21,12 @@ export const roundMiddleWare = (store) => (next) => (action) => {
          * The most up-to-date total score for the current player
          */
         const totalScore = getTotalScore(
-          currentState.round.turns.current.id,
-          currentState.scores
+          scores,
+          round.turns.current.playerID
         )
         store.dispatch(endTurnActionCreator(totalScore))
 
-        // return next(startTurnActionCreator())
-        if (currentState.end === null) {
+        if (end === null) {
           return next(startTurnActionCreator())
         } else {
           return next(finaliseRoundActionCreator())
@@ -35,7 +35,7 @@ export const roundMiddleWare = (store) => (next) => (action) => {
       break
 
     case TURN.START:
-      if (currentState.round.playersInOrder.length === 0) {
+      if (round.playersInOrder.length === 0) {
         // We've reached the end of the current round
         // Better do all the adding up for this round
         store.dispatch(finaliseRoundActionCreator())
@@ -43,18 +43,16 @@ export const roundMiddleWare = (store) => (next) => (action) => {
         // Better get everything ready for the next round
         store.dispatch(
           initialiseRoundActionCreator(
-            currentState.players.playersSeatOrder,
-            currentState.config.playOrder
+            players.playersSeatOrder,
+            config.playOrder
           )
         )
         // Now we can let the start action do its thing
       }
-      // need to get a UID for the turn,
-      // plus the name and ID of the user whose turn it is.
       break
 
     case TURN.END:
-      if (currentState.round.playersInOrder.length === (currentState.players.all.length - 1)) {
+      if (round.playersInOrder.length === (players.all.length - 1)) {
         store.dispatch(action)
         // store.dispatch({
         //   type:
--- state/reducers/scores.reducer.ts
+++ state/reducers/scores.reducer.ts
@@ -1,4 +1,5 @@
-import { ITurnComplete, IAction } from '../types'
+import { ITurnComplete, IAction, SCORE_SORT_METHOD, FILTER_BY_PROP } from '../types'
+import { pureSort, ICompare } from '../utilities/functional-sort'
 
 export const SCORE = {
   ADD: 'Add score',
@@ -19,16 +20,97 @@ const getTotalScoreReducer = (id : number) => (oldTotal:number, turn:ITurnComple
 }
 
 /**
+ * get a callback function to be used by Array.sort()
+ *
+ * @param whichScore which score should the sort be based on
+ */
+const sortByScore = (whichScore : SCORE_SORT_METHOD = SCORE_SORT_METHOD.round) : ICompare => (turnA : ITurnComplete, turnB: ITurnComplete ) : number => {
+  if (turnA.score[whichScore] < turnB.score[whichScore]) {
+    return -1
+  } else if (turnA.score[whichScore] > turnB.score[whichScore]) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+/**
+ * get a callback function to be used by Array.filter()
+ *
+ * @param whichType which type of score (ITurn) should be returned
+ * @param id        row or player UID to filter by
+ */
+const filterByType = (whichType : FILTER_BY_PROP, id: number) => (item: ITurnComplete) : boolean {
+  return (item[whichType] === id)
+}
+
+const sortTurns = (
+  filteredTurns: ITurnComplete[],
+  sortedBy : SCORE_SORT_METHOD = SCORE_SORT_METHOD.order
+) => {
+  switch (sortedBy) {
+    case SCORE_SORT_METHOD.round:
+    case SCORE_SORT_METHOD.total:
+      return pureSort(filteredTurns, sortByScore(sortedBy))
+
+    default:
+      return filteredTurns
+  }
+}
+
+
+/**
+ * Get all the turns for a player round.
+ *
+ * @param allScores all the scores (ITurns) recored so far
+ * @param id        UID of the round to be retrieved
+ * @param sortedBy  the order in which to retrieve the turns
+ */
+export const getPlayerTurns = (
+  allScores: ITurnComplete[],
+  id: number,
+  sortedBy : SCORE_SORT_METHOD = SCORE_SORT_METHOD.order
+) : ITurnComplete[] => {
+  return sortTurns(
+    allScores.filter(
+      filterByType(FILTER_BY_PROP.playerID, id)
+    ),
+    sortedBy
+  )
+}
+
+/**
+ * Get all the turns for a given round.
+ *
+ * @param allScores all the scores (ITurns) recored so far
+ * @param id        UID of the round to be retrieved
+ * @param sortedBy  the order in which to retrieve the turns
+ */
+export const getRoundTurns = (
+  allScores: ITurnComplete[],
+  id: number,
+  sortedBy : SCORE_SORT_METHOD = SCORE_SORT_METHOD.order
+) : ITurnComplete[] => {
+  return sortTurns(
+    allScores.filter(
+      filterByType(FILTER_BY_PROP.id, id)
+    ),
+    sortedBy
+  )
+}
+
+/**
+ * Get the cumulative score for a given player
  *
  * @param playerID ID for the player we want the score total for
  * @param allScores All the scores from all the player
  */
-export const getTotalScore = (playerID : number, allScores: ITurnComplete[]) => {
+export const getTotalScore = (allScores: ITurnComplete[], playerID : number) => {
   return allScores.reduce(getTotalScoreReducer(playerID), 0)
 }
 
 
-export const scoresReducer = (state = [], action: IAction) => {
+export const scoresReducer = (state : ITurnComplete[] = [], action: IAction) => {
   switch (action.type) {
     case SCORE.ADD:
       return [...state, ...action.payload.turns]
